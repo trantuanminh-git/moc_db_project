b69f494fe7824dc35d09475df1042e859ecf1de1
moctran <87555673+moctran@users.noreply.github.com>
2023-11-28 09:57:24 +0700

    Add files via upload

--- Client_page/register.js
+++ Client_page/register.js
@@ -2,18 +2,9 @@ function submitForm() {
     // Get form values
     var username = document.getElementById("exampleFirstName").value;
     var email = document.getElementById("exampleInputEmail").value;
-    var userId = document.getElementById("exampleUserID").value;
     var password = document.getElementById("exampleInputPassword").value;
     var repeatPassword = document.getElementById("exampleRepeatPassword").value;
 
-    if((username == null || username == "")
-    &&(email == null || email == "")
-    &&(userId == null || userId == "")
-    &&(password == null || password == "")
-    &&(repeatPassword == null || repeatPassword == "")){
-        alert('Please enter all register information');
-        return;
-    }else{
     // Check if password and repeat password match
     if (password !== repeatPassword) {
         alert("Password and Repeat Password do not match. Please try again.");
@@ -26,30 +17,14 @@ function submitForm() {
     var userObject = {
         username: username,
         email: email,
-        user_id: userId,
         password: password,
-        user_type: 0
+        repeatPassword: repeatPassword
     };
 
-    fetch('http://localhost:5000/register', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(userObject),
-          })
-          .then(response => response.json())
-          .then(data => {
-            if (data.success) {
-              alert('Register successful');
-              // Redirect to the login page after successful registration
-              window.location.href = "login.html";
-            } else {
-              alert('Register failed');
-            }
-          })
-          .catch((error) => {
-            console.error('Error:', error);
-          });
-}
+    // Log JSON object to console 
+    console.log(JSON.stringify(userObject));
+
+    // Redirect to the login page after successful registration
+    window.location.href = "login.html";
+    
 }
